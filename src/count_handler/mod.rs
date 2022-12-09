e33b7a44cfd8c10ae071f5d2f7a53b9aa3f238f8
Ivo Zwijsen <ivoz@unifysquare.com>
2022-12-09 08:07:27 +0100

    [Added] count map

--- src/count_handler/mod.rs
+++ src/count_handler/mod.rs
@@ -1,11 +1,11 @@
 use regex::Regex;
+use std::collections::HashMap;
 
 // This works with space separated languages only
 pub fn count_words(file: String) {
     let preprocessed_text = preprocess_text(file);
-    let split_by_space = preprocessed_text.split(" ").collect::<Vec<&str>>();
-
-    print!("{:?}", split_by_space);
+    let word_count = generate_count_struct(&preprocessed_text);
+    print!("{:?}", word_count);
 }
 
 // TODO: Refactor this
@@ -40,3 +40,20 @@ fn apply_regex(file: &String, regexp: &str, replace_with: String) -> String {
     let reg_exp = Regex::new(regexp).unwrap();
     reg_exp.replace_all(&file, replace_with).to_string()
 }
+
+fn generate_count_struct(file: &String) -> HashMap<&str, u32> {
+    let split_text = file.split(" ");
+    let mut word_count_map = HashMap::new();
+
+    for word in split_text {
+        if !word_count_map.contains_key(word) {
+            word_count_map.insert(word, 1);
+            continue;
+        }
+        word_count_map.entry(word).and_modify(|count| *count += 1);
+    }
+
+    word_count_map
+}
+
+fn sort_map() {}
