2045b07862bb5759430ad025d8166b3d6b93b67e
Ivo Zwijsen <ivoz@unifysquare.com>
2022-12-13 00:56:18 +0100

    [Added] ngram handler (it's kind of broken...)

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
-mod count_handler;
 mod io_handler;
+mod n_gram_handler;
+mod text_preprocessor;
 
-use count_handler::count_words;
-use io_handler::read_file;
+use io_handler::{get_args, read_file};
+use n_gram_handler::calculate_probability;
+use text_preprocessor::preprocess_text;
 
 fn main() {
     let file = read_file(String::from("./data/anthem_clean.txt"));
-    count_words(file);
+    let processed_file = preprocess_text(file);
+    let args = get_args();
+    let prob = calculate_probability(args[1].to_string(), processed_file, 2);
+    print!("{}", prob);
 }
