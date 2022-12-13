2fa37ce25b012500e2ec0f818bc5f3fd5fdd9d32
Ivo Zwijsen <ivoz@unifysquare.com>
2022-12-14 00:02:03 +0100

    [Added] n gram counter

--- src/count_handler/mod.rs
+++ src/count_handler/mod.rs
@@ -1,17 +1,33 @@
 use std::collections::HashMap;
 
-// This works with space separated languages only
-pub fn count_words(file: String) -> HashMap<String, u32> {
-    let word_count = generate_count_struct(&file);
-    word_count
-}
-
-fn generate_count_struct(file: &String) -> HashMap<String, u32> {
+pub fn generate_count_map(file: &String) -> HashMap<String, u32> {
     let split_text = file.split(" ");
     let mut word_count_map = HashMap::new();
 
     for word in split_text {
         if !word_count_map.contains_key(word) {
+            word_count_map.insert(word.to_string(), 1);
+            continue;
+        }
+        word_count_map
+            .entry(word.to_string())
+            .and_modify(|count| *count += 1);
+    }
+
+    word_count_map
+}
+
+pub fn generate_n_gram_count_map(file: &String, n: usize) -> HashMap<String, u32> {
+    let split_text: Vec<&str> = file.split(" ").collect();
+    let mut word_count_map = HashMap::new();
+
+    let mut index_bottom = 0;
+    for index in n..split_text.len() {
+        let word = split_text[index_bottom..index].join(" ");
+        print!("{}", word);
+        index_bottom += 1;
+
+        if !word_count_map.contains_key(&word) {
             word_count_map.insert(word.to_string(), 1);
             continue;
         }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
+mod count_handler;
 mod io_handler;
-mod n_gram_handler;
 mod text_preprocessor;
 
+use count_handler::{generate_count_map, generate_n_gram_count_map};
 use io_handler::{get_args, read_file};
-use n_gram_handler::calculate_probability;
 use text_preprocessor::preprocess_text;
 
 fn main() {
+    // Getting args
+    let args = get_args();
+    let user_input = args[1];
+
+    // Processing and counting the target file
     let file = read_file(String::from("./data/anthem_clean.txt"));
     let processed_file = preprocess_text(file);
-    let args = get_args();
-    let prob = calculate_probability(args[1].to_string(), processed_file, 2);
-    print!("{}", prob);
+    let target_count = generate_count_map(&processed_file);
+    let condition_count = generate_n_gram_count_map(&processed_file, 2);
+
+    // Calculating probability
+
+    println!("{:?}", condition_count);
 }
